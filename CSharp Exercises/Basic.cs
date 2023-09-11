82160908b603c7637e35ad545ecb47ed478275ca
arthur <arthur.bachirov@gmail.com>
2023-09-11 15:38:58 +0200

    exercise Basic 41 completed

--- CSharp Exercises/Basic.cs
+++ CSharp Exercises/Basic.cs
@@ -28,6 +28,9 @@ public class BasicExercises
 
     /**
      * Check the nearest value of 20 of two given integers and return 0 if two numbers are same
+     * 
+     * Nested ternary!
+     * 
     **/
     public static void Basic40()
     {
@@ -39,11 +42,27 @@ public class BasicExercises
         Console.WriteLine("\nWrite second number to compare: ");
         int y = Convert.ToInt32(Console.ReadLine());
 
-        int comparisonX = Math.Abs(x - REFERENCE_VALUE); // 2 - 20 = 18
-        int comparisonY = Math.Abs(y - REFERENCE_VALUE); // 18 - 20 = 2
+        int comparisonX = Math.Abs(x - REFERENCE_VALUE); // ABS(2 - 20) = 18 
+        int comparisonY = Math.Abs(y - REFERENCE_VALUE); // ABS(18 - 20) = 2
 
-        Console.WriteLine("Closest to " + REFERENCE_VALUE + " is :");
+        Console.WriteLine("Closest to " + REFERENCE_VALUE + " is :"); 
         Console.WriteLine(comparisonX == comparisonY ? 0 : comparisonX > comparisonY ? y : x); 
+    }
+
+    /**
+     * Check if a given string contains 'w' character between 1 and 3 times
+     * 
+     * Lambda expression: is a concise way to define anonymous methods or functions in programming languages
+    **/
+    public static void Basic41()
+    {
+        Console.WriteLine("Input a string (contains at least one 'w' char) : ");
+        string str = Console.ReadLine();
+
+        var count = str.Count(s => s == 'w'); // lambda expression counting how many characters in the string str are equal to the character 'w'
+        Console.WriteLine("The string contains 'w' character  between 1 and 3 times: ");
+        Console.WriteLine(count >= 1 && count <= 3);
+
     }
 
 }
