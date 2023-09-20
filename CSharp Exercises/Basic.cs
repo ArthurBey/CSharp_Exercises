614003004652c930f9dc7ba51884331b4abccd5c
arthur <arthur.bachirov@gmail.com>
2023-09-20 16:04:34 +0200

    Basic 56 palindromes completed

--- CSharp Exercises/Basic.cs
+++ CSharp Exercises/Basic.cs
@@ -1,6 +1,7 @@
 ﻿
 
 
+using System;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
@@ -47,6 +48,7 @@ public class BasicExercises
 
         Console.WriteLine("Closest to " + REFERENCE_VALUE + " is :");
         Console.WriteLine(magnitudeX == magnitudeY ? 0 : magnitudeX > magnitudeY ? y : x);
+
     }
 
     /**
@@ -68,7 +70,7 @@ public class BasicExercises
     /**
      * Create a new string where the first 4 characters will be in lower case. Upper case if string < 4 chars.
      * 
-     * Substring method
+     * Substring method : string.Substring(start index, int length) => not 'end index' !
     **/
     public static void Basic42()
     {
@@ -91,9 +93,10 @@ public class BasicExercises
      * Create a new string of every other character (odd index) from the first position of a given string
      * 
      * MAIN TAKEAWAY:
-     * - Checking for odd number with statement"i % 2 == 0"
+     * - Checking if [even] indice with statement"i % 2 == 0"
      * - you can initialize a string to "" with string.Empty
-     * 
+     * - HAD A BIG CONFUSION: ODD INDEX == EVEN num. when looking on the string character...
+     * ==> "WORD" => 'O' even here (2nd position) but index is ODD (1)  
      * 
     **/
     public static void Basic44()
@@ -107,13 +110,14 @@ public class BasicExercises
 
         var result = string.Empty;
 
-        for (var i = 0; i < str.Length; i++)
-        {
-            if (i % 2 == 0)
+        for(int i = 0; i < str.Length; i++) 
+        { 
+            if(i % 2 == 0)
             {
-                result += str.Substring(i, 1);
+                str += str.Substring(i, 1);
             }
         }
+        
         Console.WriteLine(result);
 
     }
@@ -167,8 +171,8 @@ public class BasicExercises
     public static void Basic48()
     {
         int[] nums = { 1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1};
-        Console.WriteLine((nums.Length >= 1) && (nums[0].Equals(nums[nums.Length - 1])));
-    }
+        Console.WriteLine(nums.Length >= 1 && (nums[0].Equals(nums[nums.Length -1])));
+    }  
 
     /**
      * Check if the first or the last element of the two arrays are equal
@@ -202,11 +206,11 @@ public class BasicExercises
         Console.WriteLine("\nArray contains an odd number? Result: " + OddNumberCheck(nums));
         
     }
-    public static bool OddNumberCheck(int[] num)
+    public static bool OddNumberCheck(int[] nums)
     {
-        foreach(var n in num)
+        foreach(var n in nums)
         {
-            if (n % 2 != 0)
+            if(n % 2 != 0)
             {
                 return true;
             }
@@ -215,7 +219,121 @@ public class BasicExercises
     }
 
     /**
-    * Create an array containing the middle elements of three arrays of integers
+    * Get the century from a year
+    * 
+    * 
+    * MAIN TAKEAWAY:
+    * 
+   **/
+    public static void Basic54()
+    {
+        Console.WriteLine("Enter a year: \n");
+        double year = Convert.ToInt32(Console.ReadLine());
+
+        double century = Math.Floor((year / 100) + 1);
+
+        Console.WriteLine($"{year} is part of the {century} century\n");
+    }
+
+    /**
+    * Find the pair of adjacent elements that has 'the largest product of a given array' (wtf?) which is equal to a given value
+    * 
+    * MAIN TAKEAWAY:
+    * 
+   **/
+    public static void Basic55()
+    {
+        Console.WriteLine(ArrayAdjacentElementsProduct(new int[] { 2, 4, 2, 6, 9, 3 }) == 27);
+        Console.WriteLine(ArrayAdjacentElementsProduct(new int[] { 0, -1, -1, -2 }) == 2);
+    }
+    public static int ArrayAdjacentElementsProduct(int[] inputArray)
+    {
+        int arrayIndex = 0;
+        int product = inputArray[arrayIndex] * inputArray[arrayIndex + 1];
+        
+        for (arrayIndex = 1; arrayIndex < inputArray.Length - 1 /** We did one iteration **/; arrayIndex++)
+        {
+            int currentProduct = inputArray[arrayIndex] * inputArray[arrayIndex + 1];
+            if (currentProduct > product)
+            {
+                product = currentProduct;
+            }
+        }
+
+        return product;
+    }
+
+    /**
+    * Check if a given string is a palindrome or not
+    * 
+    * MAIN TAKEAWAY: My personal attempt
+    * 
+   **/
+    public static void Basic56a()
+    {
+        Console.WriteLine("Palindrome checker: \n");
+        char[] strInput = Console.ReadLine().ToCharArray(); 
+       
+        Console.WriteLine(IsPalindrome(strInput));
+    }
+    public static bool IsPalindrome(char[] strInput)
+    {
+        for (int i = 0; i < strInput.Length; i++)
+        {
+            char strReversed = strInput[strInput.Length - i - 1];
+            if (strInput[i] != strReversed)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+    * Check if a given string is a palindrome or not
+    * 
+    * MAIN TAKEAWAY: Solution A
+    * string method Replace(" ", "");
+    * 
+   **/
+    public static void Basic56b()
+    {
+        Console.WriteLine("Palindrome checker: \n");
+        string input = Console.ReadLine();
+
+        bool isPalindrome = IsPalindrome(input);
+
+        if(isPalindrome) 
+        {
+            Console.WriteLine("Is palindrome");
+        }
+        else
+        {
+            Console.WriteLine("Is not palindrome");
+        }
+    }
+    public static bool IsPalindrome(string input)
+    {
+        //Remove spaces and convert to lowercase
+        input = input.Replace(" ", "").ToLower();
+
+        int left = 0;
+        int right = input.Length - 1; // -1 because used for index
+
+        while(left < right)
+        {
+            if (input[left] != input[right])
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    /**
+    * 
     * 
     * MAIN TAKEAWAY:
     * 
